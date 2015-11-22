aecf7219749e4445cbc60f64a7461890fde903c6
NeoCui <cuiluji@gmail.com>
2015-11-22 19:22:36 +0800

    student code update

--- student.cpp
+++ student.cpp
@@ -1,13 +1,36 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include "student.h"
 
-using namespace std;
+void Student::GetStudent(MysqlDB& mysql){
+	string sql;
+	sql = "select * from student where Name = "+name;
+	mysql.ShowRecord(sql);
+	cout<<"User Name:"<<mysql.res->getString("Name")<<endl;
+	cout<<"User ID:"<<mysql.res->getInt("ID")<<endl;
+}
 
-class Student{
-	//initialization, load\insert\delete\alter\search information from db
-	public:
-	
-	private:
+void Student::GetCur(MysqlDB& mysql){
+	string sql;
+	sql = "select * from stucur where StudentID = "+id;
+	mysql.ShowRecord(sql);
+	while(mysql.res->next()){
+		cout<<"curriculumID:"<<mysql.res->getInt("CurID")<<endl;
+	}
+}
 
-};
+void Student::AddCur(MysqlDB& mysql){
+	string curid;
+	cout<<"Input the course id you want to add:";
+	cin>>curid;
+	string sql;
+	sql = "insert into stucur(StudentID, CurID) values('"+id+","+curid;
+	mysql.ExcuteSQL(sql);
+}
+
+void Student::DeleteCur(MysqlDB& mysql){
+	string curid;
+	cout<<"Input the course id you want to delete:";
+	cint>>curid;
+	string sql;
+	sql = "delete from stucur where CurID = "+curid;
+	mysql.ExcuteSQL(sql);
+}
--- student.h
+++ student.h
@@ -1,13 +1,29 @@
+/*student.h*/
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
 #include<iostream>
 #include<string>
 #include<vector>
+
+#include "mysqldb.h"
 
 using namespace std;
 
 class Student{
 	//initialization, load\insert\delete\alter\search information from db
 	public:
+		Student(string name, int id):name(""),id(0){}
+		~Student();
+		void GetStudent(MysqlDB& mysql);
 	
-	private:
+		void GetCur(MysqlDB& mysql);
 
+		void AddCur(MysqlDB& mysql);
+
+		void DeleteCur(MysqlDB& mysql);
+
+	private:
+		string name;
+		int id;
 };
+#endif
